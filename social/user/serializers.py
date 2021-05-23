5ed826a31f08efe5b8b5ae21daf41d0d71230e68
alias <nitishkumarchauhan1996@gmail.com>
2021-05-23 06:24:25 +0530

    modified:user api
    login and register

--- social/user/serializers.py
+++ social/user/serializers.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.db.models import fields
 from rest_framework import serializers
 from .models import User
@@ -10,13 +9,12 @@ class UserSerializerLogin(serializers.Serializer):
 
 
 class UserSerializerRegister(serializers.ModelSerializer):
-    re_password = serializers.CharField()
-
+    # re_password = serializers.CharField()
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'username', 'password', ]
 
-    def validate_password(self, value):
-        if value != self.re_password:
-            raise ValidationError("the password doesn't match during register")
-        return value
+    # def validate_password(self, value):
+    #     if value != self.re_password:
+    #         raise ValidationError("the password doesn't match during register")
+    #     return value
--- social/user/views.py
+++ social/user/views.py
@@ -1,10 +1,16 @@
 import rest_framework
 from rest_framework.response import Response
-from .serializers import UserSerializerLogin
-from django.contrib.auth import login, authenticate, logout, get_user_model
-# Create your views here.
+from .serializers import UserSerializerLogin, UserSerializerRegister
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
+@api_view(["POST"])
+@permission_classes([AllowAny])
 def login(request, *args, **kwargs):
     serialized_data = UserSerializerLogin(data=request.data)
     if(serialized_data.is_valid(raise_exception=True)):
@@ -12,17 +18,33 @@ def login(request, *args, **kwargs):
         u = authenticate(email=data.get("email"),
                          password=data.get("password"))
         if u is not None:
-            login(request, u)
-            return Response({"message": "logged in successfully"}, status=200)
+            refresh = RefreshToken.for_user(u)
+            res = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+            return Response(res, status=200)
         else:
             return Response({"message": "try again"}, status=401)
 
 
+@api_view(["POST"])
+@permission_classes([AllowAny])
 def register(request, *args, **kwargs):
+    serializer = UserSerializerRegister(data=request.data)
+    if not serializer.is_valid(raise_exception=True):
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
+    res = {
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+    }
+    return Response(res, status.HTTP_201_CREATED)
 
-    return Response({"message": "registered successfully"}, status=200)
 
-
+@api_view(["POST"])
+@authentication_classes([JWTAuthentication, ])
+@permission_classes([IsAuthenticated])
 def logout(request, *args, **kwargs):
-
     return Response({"message": "logged out successfully"}, status=200)
