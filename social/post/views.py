47be97be1af64baa34ffa624a592538ed56e05e1
alias <nitishkumarchauhan1996@gmail.com>
2021-05-28 04:58:52 +0530

    backendRefine

--- social/post/views.py
+++ social/post/views.py
@@ -1,8 +1,9 @@
 from .models import Post, Like
 from .serializer import PostSerializer, PostActionSerializer, PostDisplaySerializer
 from rest_framework.decorators import api_view, authentication_classes, authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 # Create your views here.
 
@@ -13,13 +14,14 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def listPostAPI(request, *args, **kwargs):
     """
     REST API for fetching feeds at home page
     """
     posts = Post.objects.all()
     serialized_data = PostDisplaySerializer(posts, many=True)
-    return Response(serialized_data.data, status=200)
+    return Response(serialized_data.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -32,9 +34,9 @@ def creationPostAPI(request, *args, **kwargs):
     serailized_obj = PostSerializer(data=request.data)
     if serailized_obj.is_valid(raise_exception=True):
         serailized_obj.save(user=request.user)
-        return Response(serailized_obj.data, status=201)
+        return Response(serailized_obj.data, status=status.HTTP_201_CREATED)
     else:
-        return Response({"message": "the form data is overlimit or null"}, status=400)
+        return Response({"message": "the form data is overlimit or null"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
@@ -42,14 +44,12 @@ def detailPostAPI(request, post_id, *args, **kwargs):
     """
     REST API for fetching feeds detail
     """
-    status = 404
     try:
         obj = Post.objects.get(id=post_id)
         serialized_data = PostDisplaySerializer(obj)
-        status = 200
-        return Response(serialized_data.data, status=status)
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
     except:
-        return Response({"message": "NotFound"}, status=status)
+        return Response({"message": "NotFound"}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST', 'DELETE'])
@@ -65,9 +65,9 @@ def deletePostAPI(request, post_id, *args, **kwargs):
             obj.delete()
             return Response({"message": "deleted!"})
         else:
-            return Response({"message": "Forbidden action"}, status=403)
+            return Response({"message": "Forbidden action"}, status=status.HTTP_403_FORBIDDEN)
     except:
-        return Response({"message": "NotFound"}, status=404)
+        return Response({"message": "NotFound"}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST', ])
@@ -85,27 +85,27 @@ def ActionOnPostAPI(request, *args, **kwargs):
         try:
             obj = Post.objects.get(id=id)
         except:
-            return Response({"message": "POST NotFound"}, status=404)
+            return Response({"message": "POST NotFound"}, status=status.HTTP_404_NOT_FOUND)
         if action == 'like':
             if Like.objects.filter(user=request.user, post=obj).count() == 0:
                 obj.likes.add(request.user)
-                return Response({"message": "Liked!"}, status=200)
+                return Response({"message": "Liked!"}, status=status.HTTP_200_OK)
             else:
-                return Response({"message": "already liked !"}, status=200)
+                return Response({"message": "already liked !"}, status=status.HTTP_200_OK)
         elif action == 'unlike':
             if Like.objects.filter(user=request.user, post=obj).count() > 0:
                 obj.likes.remove(request.user)
-                return Response({"message": "UnLiked!"}, status=200)
+                return Response({"message": "UnLiked!"}, status=status.HTTP_200_OK)
             else:
-                return Response({"message": "already unliked !"}, status=200)
+                return Response({"message": "already unliked !"}, status=status.HTTP_200_OK)
         elif action == 'repost':
             Post.objects.create(
                 og_post=obj,
                 user=request.user,
                 content=""
             )
-            return Response({"message": "Reposted!"}, status=201)
+            return Response({"message": "Reposted!"}, status=status.HTTP_200_OK)
         else:
-            return Response({"message": f"invalid action {action}"}, status=400)
+            return Response({"message": f"invalid action {action}"}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        return Response({"message": "INVALID REQUEST"}, status=400)
+        return Response({"message": "INVALID REQUEST"}, status=status.HTTP_400_BAD_REQUEST)
